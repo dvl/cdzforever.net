48c3799eb195c4d1e727aaf3f8b93d15d3e333a9
André Luiz <contato@xdvl.info>
2014-05-26 03:03:59 -0300

    testes com angular

--- cdzforever/static/js/app.js
+++ cdzforever/static/js/app.js
@@ -1,1 +1,13 @@
-var episodiosApp = angular.module('episodiosApp', ['mainCtrl', 'episodioService']);
+function controller($scope, $http) {
+    $scope.loading = true;
+
+    $http.get('/catalogo/series/').success(function(data) {
+        $scope.series = data;
+    });
+
+    $http.get('/catalogo/episodios/').success(function(data) {
+        $scope.episodios = data;
+    });
+    
+    $scope.loading = false;
+}
