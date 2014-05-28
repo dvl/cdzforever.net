2729c80e44e7b105f430d1466414e733e1f603ed
André Luiz <contato@xdvl.info>
2014-05-28 04:13:19 -0300

    template

--- cdzforever/urls.py
+++ cdzforever/urls.py
@@ -9,7 +9,7 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
 
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^catalogo/', include('apps.catalogo.urls', namespace='catalogo')),
     url(r'^admin/', include(admin.site.urls)),
 )
