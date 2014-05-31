6f8feb96a41afefd652b0116f66b7875d55af730
André Luiz <contato@xdvl.info>
2014-05-31 20:56:50 -0300

    s3

--- cdzforever/settings/prod.py
+++ cdzforever/settings/prod.py
@@ -17,5 +17,14 @@ TEMPLATE_DEBUG = DEBUG
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'cdzforever'
+
+STATIC_URL = 'http://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+
 STATIC_ROOT = 'staticfiles'
 MEDIA_ROOT = 'media'
