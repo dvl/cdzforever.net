5a0670d0eee78931ce4599770ea95f6e179dfc04
André Luiz <contato@xdvl.info>
2014-06-05 21:41:18 -0300

    remove Cling

--- cdzforever/wsgi.py
+++ cdzforever/wsgi.py
@@ -2,6 +2,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
 
-application = Cling(MediaCling(get_wsgi_application()))
+application = get_wsgi_application()
